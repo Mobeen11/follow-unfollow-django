4cc4ac5275ab8c04b0a8e39f31cb1c232554dbec
Lycore Pvt Ltd - 6 <mobeen@globalbridgesol.com>
2016-08-24 18:02:29 +0500

    static

--- Project01/settings.py
+++ Project01/settings.py
@@ -115,7 +115,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static")
+# STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static")
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 print(STATIC_ROOT)
 
 # error in static files
