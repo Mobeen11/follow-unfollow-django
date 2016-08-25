53a861bcd3718337407db07362e5a721188d827e
Lycore Pvt Ltd - 6 <mobeen@globalbridgesol.com>
2016-08-25 18:52:21 +0500

    static

--- Project01/settings.py
+++ Project01/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 SECRET_KEY = '(awkd)((s2yy@rtlmrs_f%@fy2wudb+q@a_e8a6q!*$7+h)4%o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 TEMPLATE_DEBUG = True
 
@@ -265,9 +265,9 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 ALLOWED_HOSTS = ['*']
 
-DEBUG = True
-
-try:
-    from .local_settings import *
-except ImportError:
-    pass
+#
+#
+# try:
+#     from .local_settings import *
+# except ImportError:
+#     pass
