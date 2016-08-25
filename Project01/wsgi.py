dc09243e782e3a446bd8b0fa7fdc233c9bde2d8f
Lycore Pvt Ltd - 6 <mobeen@globalbridgesol.com>
2016-08-25 18:21:06 +0500

    cling in wsgi

--- Project01/wsgi.py
+++ Project01/wsgi.py
@@ -9,8 +9,6 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 
 import os
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Project01.settings")
-
-
-application = get_wsgi_application()
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hellodjango.settings")
+from dj_static import Cling
+application = Cling(get_wsgi_application())
