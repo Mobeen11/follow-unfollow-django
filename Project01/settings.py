786461f81fcb784811063d33b860be4d786ff1a5
Lycore Pvt Ltd - 6 <mobeen@globalbridgesol.com>
2016-09-06 11:10:12 +0500

    settings debugging False

--- Project01/settings.py
+++ Project01/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 SECRET_KEY = '(awkd)((s2yy@rtlmrs_f%@fy2wudb+q@a_e8a6q!*$7+h)4%o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-TEMPLATE_DEBUG = True
+TEMPLATE_DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -237,6 +237,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 BASE_DIR = os.path.dirname(os.path.abspath(__file__)) #+ '/..'
+
 STATIC_URL = '/static/'
 
 ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
