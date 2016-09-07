43b47e7ac3544cdf11a39e6133ff6bf1f2347158
Lycore Pvt Ltd - 6 <mobeen@globalbridgesol.com>
2016-09-07 18:25:51 +0500

    static css and debug false

--- Project01/settings.py
+++ Project01/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 SECRET_KEY = '(awkd)((s2yy@rtlmrs_f%@fy2wudb+q@a_e8a6q!*$7+h)4%o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-TEMPLATE_DEBUG = True
+TEMPLATE_DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
