22eb464af6a2fd85bea1e2b3937da66a88e414d3
MOBEEN <mobeen.ahmed11@yahoo.com>
2016-09-17 19:47:36 +0500

    settings static fixed again

--- Project01/settings.py
+++ Project01/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.staticfiles',
     'follow_unfollow',
     'facebook_image',
     'tinymce',
